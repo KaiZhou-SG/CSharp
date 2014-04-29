d1bc1ccb288d1fe379553baab67248a9d1074afe
Zhou Kai <kai.zhou.sg@gmail.com>
2014-04-29 18:03:22 +0800

    add

---
+++ ps/searching.ps1
@@ -0,0 +1,50 @@
+﻿param([string] $pathNewProgram, [string[]] $newPgmNames)
+$matched
+
+$pathNewProgram = Read-Host 'please provide the path for the new programs'
+
+Function getAllNamesOfNewPrograms($pathNewProgram)
+{
+  return Get-ChildItem $pathNewProgram
+}
+
+Function lookForExistingPrograms([string[]] $newPgmNames)
+{
+  '1 new programs are: ' + $newPgmNames
+  if ($newPgmNames.Count -eq 0)
+  {
+    $matched = $false
+    return $matched
+  }
+
+  foreach ($name in $newPgmNames)
+  {
+    'I am in the look to match'
+    if (!(Test-Path .\$name))
+    {
+      $bMatch = $false
+      $name + ' not found in current folder'
+      $matched = $false
+      return $matched
+    }
+  }
+
+  $matched = $true
+  return $matched
+}
+
+
+'fuc 2' 
+$newPrograms = getAllNamesOfNewPrograms $pathNewProgram
+'fuc 3 ' + $newPgmNames
+$matched = lookForExistingPrograms $newPrograms
+
+'fuc 4'
+if ($matched)
+{
+  'matched'
+}
+else 
+{
+  'doesn''t match'
+}
