0e8572acb051fe6746a8b9b8e7d54c85344b24ec
Zhou Kai <kai.zhou.sg@gmail.com>
2014-06-13 11:51:45 +0800

    add files

---
+++ ps/unit_GetCurrentItemFullPath.ps1
@@ -0,0 +1,24 @@
+﻿function GetCurrentItemFullPath
+{
+  param([string] $dirTargetItem)
+
+
+  return ($dirCurrent + $dirTargetItem.Substring($dirTarget.Length, $dirTargetItem.Length - $dirTarget.Length))
+}
+
+#test case:
+
+$dirTarget = 'C:\abcd'
+$dirCurrent = 'D:\efg'
+$dirTargetItem = 'C:\abcd\wxyz\abc.txt'
+
+#expected output:
+# 'D:\efg\wxyz\abc.txt'
+
+GetCurrentItemFullPath $dirTargetItem
+
+#unit test passed
+
+
+
+
