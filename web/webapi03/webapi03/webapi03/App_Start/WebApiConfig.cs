fc936e4bb28c2dee88f9267b24e78be4540859b5
kaizhou-sg <kai.zhou.sg@gmail.com>
2014-12-26 09:56:54 +0000

    add

---
+++ web/webapi03/webapi03/webapi03/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace webapi03
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{FirstName}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "CreateEmployee",
+                routeTemplate: 
+                  "api/{controller}/{action}/{Id}/{FirstName}/{LastName}/{Gender}"
+            );
+
+            
+        }
+    }
+}
---
+++ web/webapi03/webapi03/webapi03/Controllers/EmployeesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webapi02.Models;
+
+namespace webapi02.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        #region "Actions"
+        // GET api/<controller>
+        public IEnumerable<Employee> Get()
+        {
+            return new Employee().GetAllEmployees();
+        }
+
+        // GET api/<controller>/5
+        public Employee Get(int id)
+        {
+            return new Employee().GetEmployeeById(id);
+        }
+
+        // GET api/<controller>/Zhou
+        public Employee GetEmployeeByFirstName(String firstName)
+        {
+            return new Employee().GetEmployeeByFirstName(firstName);
+        }
+
+        // POST api/<controller>
+        public void Post(int id, String firstName, String lastName, String gender)
+        {
+            new Employee().CreateEmployee(new Employee()
+            {
+                Id = id,
+                FirstName = firstName,
+                LastName = lastName,
+                Gender = gender
+            }
+           );
+
+            return;
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]Employee value)
+        {
+
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+
+        }
+
+        #endregion
+    }
+}
