7876a1e6065787df1cbdd2173665d577df2a2654
kaizhou-sg <kai.zhou.sg@gmail.com>
2015-01-12 02:36:57 +0000

    add

---
+++ cmdOverride/cmdOverride/Apple.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmdOverride
+{
+    public class Apple : IFruit
+    {
+        public string SelfCheck()
+        {
+            return "Good!";
+        }
+
+    }
+}
---
+++ cmdOverride/cmdOverride/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmdOverride
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Apple apple = new Apple();
+            Console.WriteLine(apple.SelfCheck());
+
+            Console.ReadKey();
+        }
+    }
+}
